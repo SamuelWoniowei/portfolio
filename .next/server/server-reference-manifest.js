a40e25201cb57446b9150ea1aa832f4d44d366ec
Samuel Blessed Woniowei <xcellentsam@gmail.com>
2024-07-15 11:54:36 +0100

    added Dist file

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"vdg+tLkjeCqjXjTEJj2bmpwGn2Z5Pi5SYpjEzwcGv0I=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"NJeyfBULyFtOYd5C7eZlbKfNa2TA/lFuNaoiMQDZWcs=\"}"
