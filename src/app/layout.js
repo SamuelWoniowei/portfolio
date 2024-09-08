af901efe6bb9432b14c75c200b1bca06a67be551
Samuel Blessed Woniowei <xcellentsam@gmail.com>
2024-09-08 04:31:38 +0100

    updated meta-tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,10 +7,26 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Samuel Woniowei",
-  description: "Portfolio of Samuel Woniowei",
-  keywords: "Samuel Woniowei, portfolio, developer", 
+  description: "Portfolio of Samuel Woniowei, Frontend Developer",
+  keywords: "frontend developer, web developer, HTML, CSS, JavaScript, React, portfolio, nextjs",
+  author: "Samuel Woniowei",
+  openGraph: {
+    title: "Samuel Woniowei - Frontend Developer Portfolio",
+    description: "Explore the projects and skills of Samuel Woniowei, a frontend developer.",
+    url: "http://samuelwoniowei.vercel.app",
+    images: [
+      {
+        url: "https://res.cloudinary.com/shmulky/image/upload/v1704442189/S.png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Samuel Woniowei - Frontend Developer Portfolio",
+    description: "Explore the projects and skills of Samuel Woniowei, a frontend developer.",
+    images: ["https://res.cloudinary.com/shmulky/image/upload/v1704442189/S.png"],
+  },
 };
-
 export default function RootLayout({ children }) {
   return (
     <Providers>
