ed42c272491692cd0ef689209752f8ebed99b447
Samuel Blessed Woniowei <xcellentsam@gmail.com>
2024-09-08 04:34:24 +0100

    added icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import PropTypes from 'prop-types'; 
+import PropTypes from "prop-types";
 import { Providers } from "../lib/providers";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -8,11 +8,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Samuel Woniowei",
   description: "Portfolio of Samuel Woniowei, Frontend Developer",
-  keywords: "frontend developer, web developer, HTML, CSS, JavaScript, React, portfolio, nextjs",
+  keywords:
+    "frontend developer, web developer, HTML, CSS, JavaScript, React, portfolio, nextjs",
   author: "Samuel Woniowei",
   openGraph: {
     title: "Samuel Woniowei - Frontend Developer Portfolio",
-    description: "Explore the projects and skills of Samuel Woniowei, a frontend developer.",
+    description:
+      "Explore the projects and skills of Samuel Woniowei, a frontend developer.",
     url: "http://samuelwoniowei.vercel.app",
     images: [
       {
@@ -23,14 +25,22 @@ export const metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Samuel Woniowei - Frontend Developer Portfolio",
-    description: "Explore the projects and skills of Samuel Woniowei, a frontend developer.",
-    images: ["https://res.cloudinary.com/shmulky/image/upload/v1704442189/S.png"],
+    description:
+      "Explore the projects and skills of Samuel Woniowei, a frontend developer.",
+    images: [
+      "https://res.cloudinary.com/shmulky/image/upload/v1704442189/S.png",
+    ],
   },
 };
 export default function RootLayout({ children }) {
   return (
     <Providers>
       <html lang="en">
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="https://res.cloudinary.com/shmulky/image/upload/v1704442189/S.png"
+        />
         <body className={`${inter.className} bg-white`}>{children}</body>
       </html>
     </Providers>
@@ -38,5 +48,5 @@ export default function RootLayout({ children }) {
 }
 
 RootLayout.propTypes = {
-  children: PropTypes.node.isRequired, 
+  children: PropTypes.node.isRequired,
 };
