2da03880cfe7dd6ad038c00b3d8fb64f27a13e2f
Samuel Blessed Woniowei <xcellentsam@gmail.com>
2024-09-08 04:22:35 +0100

    hmmm

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "res.cloudinary.com",
-          port: "",
-          pathname: "/**",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  eslint: { ignoreDuringBuilds: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
